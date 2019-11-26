a91c0be237c71b9da14945e965a384f7447c0efd
Andreas Lang <andreasqp@gmail.com>
2019-11-27 00:42:38 +0200

    Function login +membrii

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { MembriiComponent } from './membrii/membrii.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MembriiComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/membrii/membrii.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-membrii',
+  templateUrl: './membrii.component.html',
+  styleUrls: ['./membrii.component.scss']
+})
+export class MembriiComponent implements OnInit {
+//  FirstName : string;
+//  LastName : string;
+//  Age : number;
+//  Job : string;
+//  ID : number;
+// Member =[{
+//   FirstName : "Adelin",
+//   LastName : "Chis",
+//   Age : 18,
+//   Job : "N-avem",
+//   ID : 1
+//
+//
+// }]
+//   constructor(FirstName:string, LastName:string, Age:number,Job:string, ID:number) {
+//   this.FirstName=FirstName;
+//   this.LastName=LastName;
+//   this.Age=Age;
+//   this.Job=Job;
+//   this.ID=ID;
+//   }
+// addMember(ID)
+// {
+//
+//
+// }
+// eliminateMember(){
+//
+// }
+
+
+  ngOnInit() {
+  }
+
+}
